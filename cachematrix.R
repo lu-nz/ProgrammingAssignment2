57cd2b1bb8899e7c9c35237f2246854df04aee6f
Lu <whv2012@gmail.com>
2015-11-20 23:25:22 +1300

    Lu's initial commit for programming assignment2 ; dsfa

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,50 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ##initialisation
+  inverse_x<-NULL
+  
+  ##set matrix
+  set<-function(y){
+    x<<-y
+    inverse_x<<-NULL
+  }
+  ##get matrix
+  get<-function()x
+  
+  ##cache inverse matrix
+  setInverse<-function(inverse)inverse_x<<-inverse
+  
+  ##get cached inverse matrix
+  getInverse<-function()inverse_x
+  
+  ##return a list object which contains all methods that can be used to 
+  ##get/modify attributes of this "CacheMatrix" i.e. interface APIs
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+
+
+## This function calculate inverse matrix for an "CacheMatrix"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ##retrive data in "CacheMatrix" object x
+  inverse_x<-x$getInverse()
+  
+  if(!is.null(inverse_x)){
+    ##cached inverse found, no need to calculate again
+    message("getting cached Inverse matrix")
+    return(inverse_x)
+  }
+  
+  ##no cached result,get matrix and calculate inverse
+  mx<-x$get()
+  inverse_x<-solve(mx)
+  
+  ##set cache in x
+  x$setInverse(inverse_x)
+  
+  ##return
+  inverse_x
 }
